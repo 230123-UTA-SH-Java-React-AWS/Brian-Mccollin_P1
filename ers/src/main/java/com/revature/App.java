a590599bf6d951d1282a68a332a2ca84122f88ac
BrianMccollin <mrbmccollin@hotmail.com>
2023-02-07 22:48:20 -0500

    Completed project 0

--- ers/src/main/java/com/revature/App.java
+++ ers/src/main/java/com/revature/App.java
@@ -1,17 +1,30 @@
 package com.revature;
 
-/**
- * Hello world!
- */
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.revature.controller.Controller;
+import com.revature.controller.LoginController;
+import com.revature.controller.ManagerController;
+import com.revature.controller.TicketController;
+
+import com.sun.net.httpserver.HttpServer;
+
 public final class App {
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    public static void main(String[] args) throws IOException {
+        
+        System.out.println("Starting server...");
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        server.createContext("/employee", new Controller());
+        server.createContext("/employee/login", new LoginController());
+        server.createContext("/employee/ticket", new TicketController());
+        server.createContext("/manage", new ManagerController());
+        server.setExecutor(null);
+        server.start();
     }
 }
---
+++ ers/src/main/java/com/revature/service/EmployeeService.java
@@ -0,0 +1,87 @@
+package com.revature.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.revature.models.EmployeeModel;
+import com.revature.models.TicketModel;
+import com.revature.repository.EmployeeRepo;
+
+public class EmployeeService {
+    EmployeeRepo repo = new EmployeeRepo();
+    ObjectMapper mapper = new ObjectMapper();
+    EmployeeModel newEmployee;
+    TicketModel newTicket;
+
+    public void saveEmployee(String employJson){
+
+        
+        try {
+            newEmployee = mapper.readValue(employJson, EmployeeModel.class);
+            repo.save(newEmployee);
+        } catch (JsonParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
+    }
+
+    public boolean employeelogin(String loginString){
+        boolean result= false;
+        try {
+            newEmployee = mapper.readValue(loginString, EmployeeModel.class);
+            return repo.login(newEmployee);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return result;
+    }
+
+    public void newTicket(String newTk){
+        
+        try {
+            newTicket = mapper.readValue(newTk, TicketModel.class);
+            repo.submitTicket(newTicket);
+        } catch (JsonParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
+    }
+
+    public String prevTicket(String employeetk){
+        String tkString = "";
+        newTicket = new TicketModel();
+        
+        
+        try {
+            newTicket = mapper.readValue(employeetk, TicketModel.class);
+            List<TicketModel> tkList = repo.getPrevTk(newTicket);
+            tkString = mapper.writeValueAsString(tkList);
+        } catch (JsonParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
+        return tkString;
+    }
+}
